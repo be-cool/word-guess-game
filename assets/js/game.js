62ae4e1a83e8e5e15d34b7fadda39f0005d5208c
Ben Cooley <bcooley58@gmail.com>
2019-06-03 21:05:51 -0400

    changed the boolean of gameOVer to get rid of the 'win' bug and also set a timeout delay on the alert for win and loss

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,7 @@ var currentWord = [];
 var wordIndex;
 var guessesRemaining = 0;
 var wins = 0;
-var gameOver = false;
-// create a function to display the what happens when you play onto the page
+var gameOver = true;
 
 // define how to reset the game with a function if it is won or lost or just to get it started
 function reset() {
@@ -35,7 +34,7 @@ function reset() {
 
     updateDisplay();
 }
-
+// create a function to display the what happens when you play onto the page
 function updateDisplay() {
     document.getElementById("wins").innerText = wins;
     var currentWordText = "";
@@ -68,24 +67,6 @@ function checkguess(letter) {
     }
 };
 
-// make a function to see if you win or lose
-function win() {
-    if(currentWord.indexOf("-") === -1) {
-        wins++;
-        gameOver = true;
-        alert("You won! You must be a Philly fan!!")
-
-    }
-};
-
-function lose()
-{
-    if(guessesRemaining <= 0) {
-        gameOver = true;
-    }
-}
-
-
 function guess(letter) {
     if (guessesRemaining > 0) {
         if (currentLetters.indexOf(letter) === -1) {
@@ -95,6 +76,26 @@ function guess(letter) {
     }
     
 };
+
+// make a function to see if you win or lose
+function win() {
+    // console.log(currentWord)
+    if(currentWord.indexOf("-") === -1) {
+        wins++;
+        gameOver = true;
+        setTimeout(function(){ alert("You Won! You must be a Philly Fan!"); }, 500);
+
+
+    }
+};
+
+function lose()
+{
+    if(guessesRemaining <= 0) {
+        gameOver = true;
+        setTimeout(function(){ alert("You lost. What kind of Philly Fan are you..."); }, 500);
+    }
+}
 
 document.onkeydown = function(event) {
     if(gameOver) {
